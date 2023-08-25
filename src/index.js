f29245085587db1258739bcaeb0c0aed44a516b5
Adriana-Salazar <adrianabsalazar@gmail.com>
2023-08-25 18:30:26 -0500

    funciones identificar archivos tipo markdown y leer el archivo

---
+++ src/index.js
@@ -0,0 +1,49 @@
+const fs = require("fs");
+const path = require("path");
+
+function isMarkdownFile(filePath) {
+  return path.extname(filePath).toLowerCase() === ".md";
+}
+
+function readingFile(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, "utf8", (err, data) => {
+      //if (err) throw err;
+      resolve(data);
+    });
+  });
+}
+
+function mdLinks(filePath) {
+  return new Promise((resolve, reject) => {
+    const absolutePath = path.resolve(filePath);
+    if (!isMarkdownFile(absolutePath)) {
+      reject(new Error("El archivo no es de tipo Markdown"));
+    } else {
+      //si es un archivo markdown lee el archivo
+      readingFile(absolutePath).then((data) => resolve(data));
+
+      // extraer los links que contenga el archivo
+    }
+  });
+};
+
+/*return new Promise((resolve, reject) => {
+    if (fs.existsSync(path)) {
+    } else {
+      reject("La ruta no existe");
+    }
+  });*/
+
+//const mdLinks = require("md-links");
+
+mdLinks("./README.md")
+  .then((links) => {
+    // => [{ href, text, file }, ...]
+    console.log(links);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+module.exports = mdLinks;
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,19 @@
-const mdLinks = require('../');
-
+const mdLinks = require('../index.js')
 
 describe('mdLinks', () => {
+  it('debería rechazar con un error si el argumento path no es una cadena', () => {
+    return mdLinks(123) // Pasamos un número en lugar de una cadena
+      .catch(error => { //captura el error rechazado
+        expect(error.message).toBe('El archivo no es de tipo Markdown'); // Verificamos el mensaje del error
+      });
+  });
+});
+
+
+/*describe('mdLinks', () => {
 
   it('should...', () => {
     console.log('FIX ME!');
   });
 
-});
+});*/
