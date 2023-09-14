49c1cd7bc47d859d8d782b2106713e1aedd919ec
Diana Morales <dianamorales100599@gmail.com>
2023-09-14 04:57:12 -0300

    cambios

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,24 @@
-const mdLinks = require('../');
+// // const {  } = require('../index.js');
+// const { mdLink } = require('../mdLink.js');
 
 
-describe('mdLinks', () => {
+// describe('mdLinks', () => {
+//   it('debería devolver  propiedades href, text y file', (done) => {
+//     const filePath = 'ruta/a/tu/archivo.md'; // Ruta a un archivo existente con enlaces
+//     mdLinks(filePath)
+//       .then((links) => {
+//         expect(Array.isArray(links)).toBe(true);
+//         expect(links.length).toBeGreaterThan(0);
 
-  it('should...', () => {
-    console.log('FIX ME!');
-  });
+//         links.forEach((link) => {
+//           expect(link).toHaveProperty('href');
+//           expect(link).toHaveProperty('text');
+//           expect(link).toHaveProperty('file');
+//         });
 
-});
+//         done(); // Indica que el test ha terminado
+//       })
+//       .catch((error) => {
+//         done(error); 
+//       });
+//   });
