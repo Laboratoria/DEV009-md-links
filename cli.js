bedd185996e180f3007be5fe746efef9b8aff856
CamiFlo <camila.flores.j@gmail.com>
2023-09-28 20:15:49 -0300

    --stats entrega total y unique links con colors

--- cli.js
+++ cli.js
@@ -1,17 +1,37 @@
 #!/usr/bin/env node
 
-const { mdLinks} = require('./index.js');
+const { mdLinks } = require('./index.js');
+const colors = require('colors');
 const path = process.argv[2];
-const options = process.argv.slice(3);
-
+const options = {
+  validate: process.argv.includes('--validate'),
+  stats: process.argv.includes('--stats')
+};
 
 mdLinks(path, options)
-.then((links) => {
-    console.log(links);
-})
-.catch((error) => {
+  .then((links) => {
+    if (options.stats) {
+      const totalLinks = links.length;
+      const uniqueLinks = new Set(links.map(link => link.href)); // Usar Set para obtener enlaces únicos
+
+      console.log(`Total: ${totalLinks} links`.yellow.bold)
+      console.log(`Unique: ${[...uniqueLinks].length} links`.magenta.bold); // Convertir Set a un array y mostrarlo
+    
+      if(options.validate){
+        const status = link.status
+
+        //const failLinks = links.filter(link => link.ok ==='fail').length;
+        console.log(`Fail: ${status} link`);
+      }
+    } else {
+      console.log(links);
+    }
+  })
+  .catch((error) => {
     console.error(error);
-});
+  });
+
 
 //'C:\\Users\\Laboratoria\\DataLovers'
 //'C:\\Users\\Laboratoria\\DEV009-md-links\\docs\\archivos.md'
+// 'C:\\Users\\Laboratoria\\textanalyzer'
