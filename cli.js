bad45266fb8973bd2bd0ca768e16ac3ece69643a
CamiFlo <camila.flores.j@gmail.com>
2023-10-02 12:05:24 -0300

    argumento --validate imprime en terminal resumen  linsk ok y fails

--- cli.js
+++ cli.js
@@ -4,32 +4,33 @@ const { mdLinks } = require('./index.js');
 const colors = require('colors');
 const path = process.argv[2];
 const options = {
-  validate: process.argv.includes('--validate'),
-  stats: process.argv.includes('--stats')
+    validate: process.argv.includes('--validate'),
+    stats: process.argv.includes('--stats')
 };
 
 mdLinks(path, options)
-  .then((links) => {
-    if (options.stats) {
-      const totalLinks = links.length;
-      const uniqueLinks = new Set(links.map(link => link.href)); // Usar Set para obtener enlaces únicos
+    .then((links) => {
+        if (options.stats) {
+            const totalLinks = links.length;
+            const uniqueLinks = new Set(links.map(link => link.href)); // Usar Set para obtener enlaces únicos
 
-      console.log(`Total: ${totalLinks} links`.yellow.bold)
-      console.log(`Unique: ${[...uniqueLinks].length} links`.magenta.bold); // Convertir Set a un array y mostrarlo
-    
-      if(options.validate){
-        const status = link.status
+            console.log(`Total: ${totalLinks} links`.magenta.bold);
+            console.log(`Unique: ${[...uniqueLinks].length} links`.cyan.bold); // Convertir Set a un array y mostrarlo
+        }
 
-        //const failLinks = links.filter(link => link.ok ==='fail').length;
-        console.log(`Fail: ${status} link`);
-      }
-    } else {
-      console.log(links);
-    }
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+        if (options.validate) {
+            const successLinks = links.filter(link => link.status === 200).length;
+            const failLinks = links.filter(link => link.status !== 200).length;
+            console.log(`Success: ${successLinks} links`.white.bold);
+            console.log(`Fail: ${failLinks} links`.yellow.bold);
+        } else {
+            console.log(links);
+        }
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+
 
 
 //'C:\\Users\\Laboratoria\\DataLovers'
